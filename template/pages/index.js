779c56a8a6a9d281c5f02e7c9b0684aaa52d1575
Patrick Sparks <pto1026@gmail.com>
2022-03-22 10:26:56 -0600

    finished stats section

--- template/pages/index.js
+++ template/pages/index.js
@@ -2,18 +2,21 @@ import Head from "next/head";
 import React from "react";
 import Hero from '../components/hero';
 import Navbar from '../components/navbar';
+import Stats from '../components/stats';
 
 export default function Home() {
   return (
     <React.Fragment>
       <Head>
-        <title>Winston's | Winston's personal import export service</title>
+        <title>Winston&apos;s | Winston&apos;s personal import export service</title>
       </Head>
       <Navbar />
       <main>
         <Hero />
-        <section className='section-stats'>
-          
+        <section className='section-stats u-text-center'>
+          <h2 className='heading-secondary section-stats__header'>Why Winston&apos;s?</h2>
+         <Stats />
+
         </section>
       </main>
     </React.Fragment>
